6fd476765792477abf289981186490c509a3a512
Pierre Rolland <prolland@lafourchette.com>
2016-12-06 17:16:00 +0100

    Refactored normalizer setAttributeValue

--- Normalizer/RecursiveObjectNormalizer.php
+++ Normalizer/RecursiveObjectNormalizer.php
@@ -42,31 +42,61 @@ class RecursiveObjectNormalizer extends ObjectNormalizer
     protected function setAttributeValue($object, $attribute, $value, $format = null, array $context = [])
     {
         try {
-            if ($this->reflectionClasses[get_class($object)]->hasProperty($attribute)) {
-                $reflectionProperty = $this->reflectionClasses[get_class($object)]->getProperty($attribute);
-                if ($type = $this->getAnnotationReader()->getPropertyAnnotation($reflectionProperty, Type::class)) {
-                    if (substr($type->name, -2) === '[]' && is_array($value)) {
-                        $denormalized = [];
-                        $class = substr($type->name, 0, -2);
-                        foreach ($value as $item) {
-                            $denormalized[] = $this->denormalize($item, $class, $format, $context);
-                        }
-                        $this
-                            ->propertyAccessor
-                            ->setValue($object, $attribute, $denormalized);
-                        return;
-                    }
-                    $denormalized = $this->denormalize($value, $type->name, $format, $context);
-                    $this
-                        ->propertyAccessor
-                        ->setValue($object, $attribute, $denormalized);
-                    return;
-                }
+            if ($this->hasProperty($object, $attribute) && ($type = $this->getTypeAnnotation($object, $attribute))) {
+                $value = $this->getDenormalizedValue($type, $value, $format, $context);
             }
             $this->propertyAccessor->setValue($object, $attribute, $value);
         } catch (NoSuchPropertyException $exception) {
             // Properties not found are ignored
         }
+    }
+
+    /**
+     * @param Type $type
+     * @param mixed $value
+     * @param string $format
+     * @param array $context
+     *
+     * @return array|object
+     */
+    protected function getDenormalizedValue(Type $type, $value, $format, $context)
+    {
+        if (substr($type->name, -2) === '[]' && is_array($value)) {
+            $denormalized = [];
+            $class = substr($type->name, 0, -2);
+            foreach ($value as $item) {
+                $denormalized[] = $this->denormalize($item, $class, $format, $context);
+            }
+
+            return $denormalized;
+        }
+
+        return $this->denormalize($value, $type->name, $format, $context);
+    }
+
+    /**
+     * @param object $object
+     * @param string $attribute
+     *
+     * @return null|Type
+     */
+    protected function getTypeAnnotation($object, $attribute)
+    {
+        return $this->getAnnotationReader()->getPropertyAnnotation(
+            $this->reflectionClasses[get_class($object)]->getProperty($attribute),
+            Type::class
+        );
+    }
+
+    /**
+     * @param object $object
+     * @param string $attribute
+     *
+     * @return bool
+     */
+    protected function hasProperty($object, $attribute)
+    {
+        return $this->reflectionClasses[get_class($object)]->hasProperty($attribute);
     }
 
     /**
